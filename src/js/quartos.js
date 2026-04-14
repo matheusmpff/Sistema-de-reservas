ff20fc40280ca2854ea3c59d7ae716efd5ac4327
João Pedro Castelli <jotapcastelli@gmail.com>
2026-04-14 19:52:37 -0300

    add navbar and progress bar

--- src/js/quartos.js
+++ src/js/quartos.js
@@ -8,6 +8,7 @@ import "@splidejs/splide/css";
 import QuartoCasalImg from "images/quartos/QuartoCasal.png";
 import QuartoTriploImg from "images/quartos/QuartoTriplo.png";
 import QuartoQuadruploImg from "images/quartos/QuartoQuadruplo.png";
+import Logo from "images/logo.png";
 
 new Splide( ".splide", {
   perPage: 2,
@@ -15,7 +16,7 @@ new Splide( ".splide", {
 } ).mount();
 //  arrowPath: 'M11 20.3152L19.3152 12M11 20.3152H27.6304M11 20.3152L19.3152 27.4425',
 
-
+document.querySelector("#logo").src = Logo;
 document.querySelector("#quarto-casal-aba img").src = QuartoCasalImg;
 document.querySelector("#quarto-triplo-aba img").src = QuartoTriploImg;
 document.querySelector("#quarto-quadruplo-aba img").src = QuartoQuadruploImg;
