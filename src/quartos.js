26f4d37390827d5bf3dfd99eff2ce3888cf40f25
João Pedro Castelli <jotapcastelli@gmail.com>
2026-04-14 15:12:47 -0300

    add HtmlPlugin and one more room to carousel, reorganize directory

--- src/quartos.js
+++ src/quartos.js
@@ -1,4 +1,18 @@
-import Splide from '@splidejs/splide'
+import Splide from '@splidejs/splide';
+import './css/quartos.css';
+import '@splidejs/splide/css';
+import QuartoCasalImg from './images/quartos/QuartoCasal.png';
+import QuartoTriploImg from './images/quartos/QuartoTriplo.png';
+import QuartoQuadruploImg from './images/quartos/QuartoQuadruplo.png'
 
-new Splide( '.splide' ).mount();
+new Splide( '.splide', {
+  perPage: 2,
+  perMove: 1,
+} ).mount();
 //  arrowPath: 'M11 20.3152L19.3152 12M11 20.3152H27.6304M11 20.3152L19.3152 27.4425',
+
+
+document.querySelector('#quarto-casal-aba img').src = QuartoCasalImg;
+document.querySelector('#quarto-triplo-aba img').src = QuartoTriploImg;
+document.querySelector('#quarto-quadruplo-aba img').src = QuartoQuadruploImg;
+//document.querySelector('#QuartoCasal').src = QuartoCasalImg;
