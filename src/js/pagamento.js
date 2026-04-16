fa1107ccb5220b66b326a3fb631291b469d5be9e
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-04-16 16:38:06 -0300

    Create functionality to delete options in the card

--- src/js/pagamento.js
+++ src/js/pagamento.js
@@ -8,3 +8,22 @@ import CarrinhoDrawing from "images/pagamento/carrinho.png"
 document.querySelector("#logo").src = Logo;
 //document.querySelector('#QuartoCasal').src = QuartoCasalImg;
 document.querySelector("#reserva-carrinho").src = CarrinhoDrawing;
+
+let lixos = document.querySelectorAll(".bi-trash-fill")
+
+for(let lixo of lixos){
+    lixo.addEventListener("click",(e)=>{
+        lixo = e.target;
+        lixo.parentElement.parentElement.parentElement.remove();
+        if(document.querySelectorAll(".bi-trash-fill").length === 0){
+            document.querySelector("#reserva-botao").remove()
+
+            let opcoes = document.querySelector("aside")
+            let divElement = document.createElement("div")
+            divElement.innerHTML = "Carrinho vazio"
+            divElement.classList.add("vazio")
+            opcoes.appendChild(divElement)
+            opcoes.style.minHeight = "200px";  
+        }
+    })
+}
