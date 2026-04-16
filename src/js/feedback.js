7c8b2bdea5baae4975b13694049051fa2715d19b
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-04-16 10:48:15 -0300

    Adjust feedback forms structure to prevent leak of elements in the design

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -12,41 +12,33 @@ let previa = document.querySelector("#previa");
 
 inputImg.addEventListener("change", () => {
 
-  while(previa.firstChild != null){
+  while (previa.firstChild != null) {
     previa.removeChild(previa.firstChild)
-  } 
+  }
 
   let arquivos = inputImg.files;
 
-  let count = 20
+  let count = 12
   for (let img of arquivos) {
-    if(count == 0){
+    if (count == 0) {
       break;
     }
-    count = count -1;
+    count = count - 1;
     let imgElement = document.createElement("img");
     imgElement.src = URL.createObjectURL(img);
 
-    if(window.innerWidth<=700){
-        imgElement.style.width = "12px";
-        imgElement.style.height = "12px";
-    }
-    if(window.innerWidth> 700 && window.innerWidth<=1000){
-      imgElement.style.width = "16px";
-      imgElement.style.height = "16px";
-    }
-    else{
-        imgElement.style.width = "20px";
-        imgElement.style.height = "20px";
-    }
-    
 
-    
+    imgElement.style.width = "40px";
+    imgElement.style.height = "40px";
+
+
+
+
 
     previa.appendChild(imgElement);
 
   }
-    let p = document.createElement("p")
-    p.innerText = "Para mudar as imagens a serem enviadas basta selecioná-las novamente pelo botão"
-    previa.appendChild(p);
+  let p = document.createElement("p")
+  p.innerText = "Para mudar as imagens a serem enviadas basta selecioná-las novamente pelo botão"
+  previa.appendChild(p);
 });
