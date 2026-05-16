710fb511c1e700328266390072118e68bdb54dd9
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-05-16 12:36:49 -0300

    Adjust p tag size and adjust color to use variables in css

--- new-repo/Ybitu-Reservas/src/pages/Cadastro.tsx
+++ new-repo/Ybitu-Reservas/src/pages/Cadastro.tsx
@@ -7,7 +7,7 @@ export default function Cadastro() {
         <div className="flex flex-col min-h-screen justify-between">
             <div className="main_container">
                 <div className="esquerda">
-                    <h2 className="text-white bg-[#1F6F50] px-2 py-0.5">CRIE A SUA CONTA</h2>
+                    <h2 className="text-white bg-[var(--cor-primaria)] px-2 py-0.5">CRIE A SUA CONTA</h2>
                     <p className=" mt-5 mb-3 text-white ">Registre-se agora para garantir:</p>
                     <ol className="list-decimal">
                         <li className="li_style">Acesso completo as possíveis reservas</li>
@@ -18,8 +18,8 @@ export default function Cadastro() {
                 <div>
                     <div className="titulo ">
                         <div className="font-bold text-4xl">FAZER CADASTRO</div>
-                        <p className=" mt-4 max-w-100 md:max-w-120 lg:mt-8">Realize seu cadastro para tirar proveito de todos os benefícios de nosso sistema de reserva. </p>
-                        <p className=" mt-3 mb-9 max-w-100 md:max-w-120">Para isso basta indicar as informações no formulário abaixo:</p>
+                        <p className="text-lg mt-4 max-w-100 md:max-w-120 lg:mt-8">Realize seu cadastro para tirar proveito de todos os benefícios de nosso sistema de reserva. </p>
+                        <p className="text-lg mt-3 mb-9 max-w-100 md:max-w-120">Para isso basta indicar as informações no formulário abaixo:</p>
                     </div>
                     <div className="formulario">
                         <div><img src="src/assets/logo_branca.png" alt="" width="40" /></div>
