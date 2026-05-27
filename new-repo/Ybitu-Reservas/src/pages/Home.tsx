764ee921a20f7ced5953aafca13720ab8b6e8a98
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-05-27 11:21:34 -0300

    Extend home page to have description and contact section

--- new-repo/Ybitu-Reservas/src/pages/Home.tsx
+++ new-repo/Ybitu-Reservas/src/pages/Home.tsx
@@ -3,10 +3,12 @@ import { Award } from "lucide-react";
 import { Star } from "lucide-react";
 import { Infinity } from "lucide-react";
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router";
+import ContactForm from "../components/ContactForm";
 
 export default function Home() {
     return (
-        <div className="container mx-auto h-screen">
+        <div className="container mx-auto">
             <div className="overflow-hidden flex flex-col lg:flex-row relative mt-12">
                 <div className="xl:ml-20 2xl:ml-30 flex flex-col justify-center items-center p-6 md:p-12">
                     <div className="text-6xl text-center text-[var(--cor-terciaria)]">Pousada</div>
@@ -33,21 +35,99 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <div className=" grid_div">
-                        <div className="bg-[url('src/assets/home/1.png')] grid_element img1_position"></div>
-                        <div className="bg-[url('src/assets/home/2.png')] grid_element img2_position"></div>
-                        <div className="bg-[url('src/assets/home/3.png')] grid_element img3_position"></div>
-                        <div className="bg-[url('src/assets/home/4.png')] grid_element img4_position"></div>
-                        <div className="bg-[url('src/assets/home/5.png')] grid_element img5_position"></div>
+                        <div className="bg-[url('src/assets/home/1.png')] grid_element_home img1_position"></div>
+                        <div className="bg-[url('src/assets/home/2.png')] grid_element_home img2_position"></div>
+                        <div className="bg-[url('src/assets/home/3.png')] grid_element_home img3_position"></div>
+                        <div className="bg-[url('src/assets/home/4.png')] grid_element_home img4_position"></div>
+                        <div className="bg-[url('src/assets/home/5.png')] grid_element_home img5_position"></div>
                     </div>
-                    <button className="mt-10 lg:mt-0 font-semibold flex bg-[var(--cor-primaria)] py-5 rounded px-5 text-[var(--cor-background)]">
-                        VENHA CONHECER MAIS
-                        <ArrowRight className="ml-2"></ArrowRight>
-                    </button>
+                    <Link to="/Pousada">
+                        <button className="cursor-pointer mt-10 lg:mt-0 font-semibold flex bg-[var(--cor-primaria)] py-5 rounded px-5 text-[var(--cor-background)]">
+                            VENHA CONHECER MAIS
+                            <ArrowRight className="ml-2"></ArrowRight>
+                        </button>
+                    </Link>
                 </div>
 
             </div>
 
-            
+            <div className="flex flex-col mt-20 mr-4 ml-4">
+                <div className="title_section">Nossa Pousada</div>
+                <div className="home_description">
+                    <div className="bg-[url('src/assets/home/quartos.png')] home_description_img"></div>
+                    <div className="mt-4">
+                        <p className="text-2xl font-semibold">Quartos Disponiveis</p>
+                        <p className="mt-4 max-w-145">
+                            Temos variados tipos de quartos arrumados da melhor forma possível para atender suas necessidades.
+                            Acesse agora a sessão de reservas para visualizar todas as opções disponíveis.
+                        </p>
+                        <div className="flex justify-end">
+                            <Link to="/Data">
+                                <button className="btn_reserva">
+                                    VERIFICAR RESERVAS
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className=" home_description mt-8">
+                    <div className="bg-[url('src/assets/home/refeicao.png')] home_description_img"></div>
+                    <div className="mt-4">
+                        <p className="text-2xl font-semibold" >Café da Manhã Incluso</p>
+                        <p className="mt-4 max-w-145">
+                            Proporcionamos uma refeição robusta de café da manhã para nossos hóspedes, trazendo grande
+                            variedade de opções. Conheça agora as opções de reservas disponíveis!
+                        </p>
+                        <div className="flex justify-end ">
+                            <Link to="/Data">
+                                <button className="btn_reserva">
+                                    VERIFICAR RESERVAS
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="home_description mt-8">
+                    <div className="bg-[url('src/assets/home/vista.png')] home_description_img"></div>
+                    <div className="mt-4">
+                        <p className="text-2xl font-semibold">Excelente Localização</p>
+                        <p className="mt-4 max-w-145">
+                            A poucos metros da praia, a localização da pousada proporciona vistas incríveis, como também, conforto e comodidade
+                            aos hóspedes.
+                        </p>
+                        <p className="mt-2 max-w-145">
+                            A pousada Ybitu é a melhor escolha para quem quer aproveitar a praia e os benefícios
+                            da pousada durante a estadia. Clique no botão abaixo para iniciar sua reserva!
+                        </p>
+                        <div className="flex justify-end">
+                            <Link to="/Data">
+                                <button className="btn_reserva">
+                                    VERIFICAR RESERVAS
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="px-4 mb-12">
+                <div className="title_section mt-12">Contato</div>
+                <div className="flex flex-col lg:flex-row justify-center items-center">
+                    <div className="lg:mr-10">
+                        <div className="text-3xl font-bold mb-10 ">Estamos a disposição para ajudar</div>
+                        <p className="max-w-150">Se alguma duvida persistir, entre em contato conosco!Estamos disponíveis para te atender e sanar todas as suas perguntas!!</p>
+                        <p className="mt-4 max-w-150"> Acesse nossa pagina de contatos para ficar por dentro de todas as opções de comunicação disponíveis.</p>
+                        <Link to="/Contato">
+                            <button className="btn_reserva mx-auto mb-12">
+                               PÁGINA DE CONTATO
+                            </button>
+                        </Link>
+                    </div>
+                    <ContactForm></ContactForm> 
+
+                </div>
+            </div>
+
         </div>
     );
 };
