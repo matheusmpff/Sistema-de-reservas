a4a9b3898bc6f6b2a544b6c3d17898392bd2e5a7
Joao Pedro Castelli <jotapcastelli@gmail.com>
2026-06-05 10:18:17 -0300

    add delete button on Hospedes list

--- Ybitu-Reservas/src/pages/Hospedes.tsx
+++ Ybitu-Reservas/src/pages/Hospedes.tsx
@@ -1,32 +1,37 @@
 // internal components
-import { useState } from "react";
 import BarraProgresso from "../components/BarraProgresso";
+import { HospedeType, hTypeToString, type stateOp } from "../types.ts";
+
+import { useState } from "react";
+import { Trash } from "lucide-react";
 
 // css
 import "../styles/Hospedes.scss";
 
-enum HospedeType {
-  Responsavel,
-  Adulto,
-  Crianca,
-}
 
 function HospInput(props: {title: string, iType: string, options?: string[]}) {
-  let input_elem = <input type={props.iType}/>;
+  let input_elem = <input type={props.iType} />;
 
   if(props.iType == "date") {
+    // the max value for date is today
     input_elem = <input type={props.iType} max={new Date().toISOString().split("T")[0]} />
-  }
-
-  if (props.iType == "select" && props.options != null) {
-    const ops = props.options.map((op) => <option value={op.toLocaleLowerCase()}>{op}</option>);
-    input_elem = <select id={props.title.toLocaleLowerCase()}>{ops}</select>;
   }
 
   return(
     <div className="hospede-input">
       <p>{props.title}</p>
       {input_elem}
+    </div>
+  )
+}
+
+function HospInputSelect(props: {title: string, options: string[]}) {
+  const ops = props.options.map((op) => <option value={op.toLocaleLowerCase()}>{op}</option>);
+
+  return(
+    <div className="hospede-input">
+      <p>{props.title}</p>
+      <select id={props.title.toLocaleLowerCase()}>{ops}</select>
     </div>
   )
 }
@@ -39,7 +44,7 @@ function ResponsavelDados() {
       <HospInput title={"Email"} iType={"email"}/>
       <div className="two-hosp-inputs">
         <HospInput title={"Data de nascimento"} iType={"date"}/>
-        <HospInput title={"Sexo"} iType={"select"} options={["Masculino", "Feminino"]}/>
+        <HospInputSelect title={"Sexo"} options={["Masculino", "Feminino"]}/>
       </div>
       <HospInput title={"Telefone"} iType={"tel"}/>
     </main>
@@ -50,13 +55,13 @@ function AdultoDados() {
   return(
     <main className="hospede-dados">
       <h2>Adulto</h2>
-      <HospInput title={"Nome"} iType={"text"}/>
-      <HospInput title={"Email"} iType={"email"}/>
+      <HospInput title={"Nome"} iType={"text"} />
+      <HospInput title={"Email"} iType={"email"} />
       <div className="two-hosp-inputs">
-        <HospInput title={"Data de nascimento"} iType={"date"}/>
-        <HospInput title={"Sexo"} iType={"select"} options={["Masculino", "Feminino"]}/>
+        <HospInput title={"Data de nascimento"} iType={"date"} />
+        <HospInputSelect title={"Sexo"} options={["Masculino", "Feminino"]} />
       </div>
-      <HospInput title={"Telefone"} iType={"tel"}/>
+      <HospInput title={"Telefone"} iType={"tel"} />
     </main>
   );
 }
@@ -65,31 +70,67 @@ function CriancaDados() {
   return(
     <main className="hospede-dados">
       <h2>Criança</h2>
-      <HospInput title={"Nome"} iType={"text"}/>
+      <HospInput title={"Nome"} iType={"text"} />
       <div className="two-hosp-inputs">
-        <HospInput title={"Data de nascimento"} iType={"date"}/>
-        <HospInput title={"Sexo"} iType={"select"} options={["Masculino", "Feminino"]}/>
+        <HospInput title={"Data de nascimento"} iType={"date"} />
+        <HospInputSelect title={"Sexo"} options={["Masculino", "Feminino"]} />
       </div>
-      <HospInput title={"Nome do responsável"} iType={"text"}/>
-      <HospInput title={"Telefone do responsável"} iType={"tel"}/>
+      <HospInput title={"Nome do responsável"} iType={"text"} />
+      <HospInput title={"Telefone do responsável"} iType={"tel"} />
     </main>
   );
 }
 
+function HospListItem(props: {hType: HospedeType, name: string, selected: boolean, click: stateOp<void>, delFn: stateOp<void>}) {
+  let sel = "";
+  if (props.selected) {
+    sel = " border-4 border-(--cor-primaria)"
+  }
+
+  const abc = (e: React.MouseEvent) => {
+    props.delFn();
+    e.stopPropagation()
+  }
+
+  return(
+    <li>
+      <div onClick={() => props.click()} className={"hosp-list-item" + sel}>
+        <h3>{hTypeToString(props.hType)}</h3>
+        <p>{props.name}</p>
+        {props.hType != HospedeType.Responsavel && <Trash className="mouse-reaction" onClick={(e) => abc(e)} />}
+      </div>
+    </li>
+  )
+}
+
+// return the type of hospede selected
+function hospSelDisplay(selected: HospedeType) {
+  if (selected == HospedeType.Responsavel) {
+    return <ResponsavelDados />;
+  }
+  if (selected == HospedeType.Adulto) {
+    return <AdultoDados />;
+  }
+  if (selected == HospedeType.Crianca) {
+    return <CriancaDados />;
+  }
+}
+
 export default function Hospedes() {
-  const [hospList, setHospList] = useState([HospedeType.Responsavel]);
+  const [hospList, setHospList] = useState([HospedeType.Responsavel] as HospedeType[]);
   const [hospSelected, setHospSelected] = useState(0);
 
-  const hospComp = () => {
-    if (hospList[hospSelected] == HospedeType.Responsavel) {
-      return <ResponsavelDados />;
+  function removeHosp(index: number) {
+    if (hospSelected == index) {
+      setHospSelected(hospSelected - 1)
     }
-    if (hospList[hospSelected] == HospedeType.Adulto) {
-      return <AdultoDados />;
-    }
-    if (hospList[hospSelected] == HospedeType.Crianca) {
-      return <CriancaDados />;
-    }
+
+    setHospList(hospList.filter((_, i) => i != index));
+  }
+
+  function hospListDisplay(list: HospedeType[]) {
+    return list.map((hospede, i) =>
+      <HospListItem key={i} hType={hospede} name="a" selected={i == hospSelected} click={() => setHospSelected(i)} delFn={() => removeHosp(i)} />)
   }
 
   const addChild = () => {
@@ -109,15 +150,15 @@ export default function Hospedes() {
       <h1 className="mb-6">Preencha os dados dos hóspedes:</h1>
       <div id="hospede-div">
         <div id="hospede-main">
-          {hospComp()}
-          <div className="hosp-dados-botao">
+          {hospSelDisplay(hospList[hospSelected])}
+          <div className="flex justify-evenly py-10">
             <button onClick={addAdult}>Adicionar adulto</button>
             <button onClick={addChild}>Adicionar criança</button>
           </div>
         </div>
 
         <div id="hospede-lista">
-          {hospList}
+          {hospListDisplay(hospList)}
         </div>
       </div>
     </>
--- Ybitu-Reservas/src/types.ts
+++ Ybitu-Reservas/src/types.ts
@@ -9,3 +9,33 @@ export type ResumoData = {
 }
 
 export type ReservaID = {id: string};
+
+const HospedeType = {
+  Responsavel: 0,
+  Adulto: 1,
+  Crianca: 2,
+} as const;
+
+type HospedeType = (typeof HospedeType)[keyof typeof HospedeType];
+
+// map the HospedeType to a string
+function hTypeToString(hType: HospedeType) {
+  if (hType == HospedeType.Responsavel) {
+    return "Responsável";
+  }
+  if (hType == HospedeType.Adulto) {
+    return "Adulto";
+  }
+  if (hType == HospedeType.Crianca) {
+    return "Criança";
+  }
+}
+
+type HospedeDados = {
+  hType: HospedeType,
+  hName: string,
+  birthDate: Date,
+  phoneNumber: string,
+}
+
+export { HospedeType, hTypeToString, type HospedeDados };
