e63e24e889cc05e3fdc5ba27fe15494409c7fa42
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-15 19:21:17 -0300

    Create change page option using components

--- Ybitu-Reservas/src/pages/Usuario.tsx
+++ Ybitu-Reservas/src/pages/Usuario.tsx
@@ -1,20 +1,23 @@
 import "../styles/Usuario.css";
 import { User } from "lucide-react";
 import { BedDouble } from "lucide-react";
-import { UserCircle2 } from "lucide-react";
 
+import { useState } from "react";
+import Profile from "../components/Profile";
+import MinhasReservas from "../components/MinhasReservas";
 
 export default function Usuario() {
+    const [mostrar, setMostrar] = useState(true)
     return (
         <div className="container mx-auto mt-20 flex flex-col md:flex-row min-h-[70vh]">
             <aside className=" min-w-fit flex flex-col items-center" >
                 <div className="font-bold text-3xl mb-6">Conta do Usuário</div>
                 <div className="flex md:flex-col">
-                    <div className="user_nav_option user_option_activate">
+                    <div onClick={() => setMostrar(true)} className={`user_nav_option ${mostrar ? "user_option_activate" : ""}`}>
                         <User size={24} />
                         <p>Usuário</p>
                     </div>
-                    <div className="user_nav_option ml-4 md:ml-0 md:mt-4">
+                    <div onClick={() => { setMostrar(false); }} className={`user_nav_option ${!mostrar ? "user_option_activate" : ""} ml-4 md:ml-0 md:mt-4`}>
                         <BedDouble size={24} />
                         <p>Reservas</p>
                     </div>
@@ -22,40 +25,8 @@ export default function Usuario() {
 
             </aside>
             <div className="w-full px-4 pt-16 ">
-                <div className="" >
-                    <div className="flex justify-between items-center lg:justify-center lg:gap-80 xl:gap-100">
-                        <div className="flex items-center justify-center ">
-                            <UserCircle2 size={60}></UserCircle2>
-                            <div className="ml-3 ">
-                                <div className="text-xl">Donatello da Silva</div>
-                                <div className="text-[12px]">donatellodasilva@gmail.com</div>
-                            </div>
-
-                        </div>
-                        <button className="p-2 w-fit h-fit rounded bg-red-500 text-[var(--cor-background)] ">Apagar conta</button>
-                    </div>
-                </div>
-                <div className="mt-12">
-                    <form className="grid grid-cols-2 max-w-fit gap-10  mx-auto" action="">
-                        <div className="user_form_div">
-                            <label htmlFor="nome">Nome:</label>
-                            <input className="user_form_option" type="text" id="nome"  value={123456} />
-                        </div>
-                        <div className="user_form_div">
-                            <label htmlFor="email">E-mail:</label>
-                            <input className="user_form_option" type="email" id="email" value={123456} />
-                        </div>
-                        <div className="user_form_div">
-                            <label htmlFor="senha">Senha:</label>
-                            <input className="user_form_option" type="password" id="senha" value={123456} />
-                        </div>
-                    </form>
-
-                    <div className="mt-10 flex items-center justify-center">
-                        <button className=" p-2 btn_primary">Alterar Dados</button>
-                        
-                    </div>
-                </div>
+                {mostrar && <Profile></Profile>}
+                {(!mostrar) && <MinhasReservas />}
             </div>
         </div>
 
