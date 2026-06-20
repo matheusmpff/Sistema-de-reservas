596224bc693b5649963928547566ec931bdd0e9e
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-20 18:10:55 -0300

    Move routes to User.ts

--- Ybitu-Backend/src/routes/MainRouter.ts
+++ Ybitu-Backend/src/routes/MainRouter.ts
@@ -3,6 +3,7 @@ import { createUser, loginUser, userData } from "../services/User.js";
 import { Auth } from "../middlewares/Auth.js";
 import jwt from "jsonwebtoken";
 import { type LoginInput, isSignupInput } from "../types.js";
+import userRouter from "./User.js";
 
 const secret = process.env.JWT_SECRET_KEY;
 console.log(secret)
@@ -11,65 +12,13 @@ if (!secret) {
 }
 
 const MainRouter = express.Router();
+MainRouter.use("/user", userRouter);
 
 MainRouter.get("/",Auth.private, (_req, res) => {
     res.json({ msg: "Oiii" });
 });
 
 // User tries to login
-MainRouter.post("/user/login", async (req: {body: LoginInput} , res) => {
-    const login_input = req.body;
 
-     try {
-        const user = await loginUser(login_input);
-
-        if (user) {
-            const token = jwt.sign({email: req.body.email}, secret, {expiresIn: "1h"});
-            return res.json({ msg: "Login realizado com sucesso", token });
-        }
-        return res.json({ msg: "Dados inválidos"});
-
-    } catch (err) {
-        console.error(JSON.stringify(err, null, 2));
-        throw err;
-    }
-
-});
-
-// User tries to signup his info with email and password
-MainRouter.post("/user", async (req, res) => {
-    console.log("a rota foi chamada");
-    const userData = req.body;
-
-    if (isSignupInput(userData)) {
-        try {
-            const user = await createUser(userData);
-
-            if (user) {
-                return res.status(201).json({ msg: "Cadastrado com Sucesso" });
-            }
-            return res.status(400).json({ msg: "Usuário já cadastrado" });
-
-        } catch (err) {
-            console.error(JSON.stringify(err, null, 2));
-            throw err;
-        }
-    }
-    return res.status(400).json({ msg: "Campos obrigatorios faltando." });
-})
-
-// Client wants the data for a user with requested email
-MainRouter.get("/user/data", async (req: {query: {email: string}}, res) => {
-    const email = req.query.email;
-    console.log("ROTA /user/data FOI CHAMADA");
-    console.log("EMAIL:", email);
-
-     try {
-        res.json( await userData(email));
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({error: "Erro do servidor"});
-    }
-});
 
 export default MainRouter;
