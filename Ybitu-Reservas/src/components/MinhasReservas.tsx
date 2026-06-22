4ec93961c1c648121565a62d605c077a03cd1656
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-22 17:36:10 -0300

    When quantity of bookings is 0 it will show a text instead of table in profile page

--- Ybitu-Reservas/src/components/MinhasReservas.tsx
+++ Ybitu-Reservas/src/components/MinhasReservas.tsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import ActiveTable from "./ActiveTable";
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
 
 export default function MinhasReservas() {
     const { logout, userEmail } = useAuth()
-    const [booking,setBooking] = useState([])
+    const [booking, setBooking] = useState([])
     const navigate = useNavigate();
     useEffect(() => {
         const bookingHandler = async () => {
             if (userEmail != "") {
-                const response = await fetch(`http://localhost:3000/user/booking`,{credentials:"include"});
+                const response = await fetch(`http://localhost:3000/user/booking`, { credentials: "include" });
                 const data = await response.json()
                 setBooking(data.booking);
             }
@@ -21,12 +22,20 @@ export default function MinhasReservas() {
 
         }
         bookingHandler();
-    },[]);
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center" >
             <div className="text-[var(--cor-primaria)] text-5xl font-bold mb-8">Reservas Ativas</div>
-            <ActiveTable listaReserva={booking} />
+            {
+                booking.length != 0 ? <ActiveTable listaReserva={booking} />
+                    : <div>
+                        <p>
+                            Pelo visto não existem reservas ativas.
+                           Caso desejar, <Link to="/Data" className="text-[var(--cor-primaria)]"> venha agora reservar conosco!</Link>
+                        </p>
+                    </div>
+            }
         </div>
     );
 };
