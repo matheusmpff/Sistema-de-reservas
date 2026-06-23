1ebc80b3b9019fc2e7dc6a8df7e38aa69b259eb4
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-23 13:24:39 -0300

    Create route to send email to Ybitu google account

--- Ybitu-Backend/src/routes/MainRouter.ts
+++ Ybitu-Backend/src/routes/MainRouter.ts
@@ -5,11 +5,22 @@ import quartosRouter from "./QuartosAdmin.js";
 import pessoaRouter from "./Pessoa.js";
 import reservaRouter from "./Reserva.js";
 import feedbackRouter from "./Feedback.js";
+import nodemailer from "nodemailer";
 
 const secret = process.env.JWT_SECRET_KEY;
 if (!secret) {
     throw new Error("JWT_SECRET_KEY não configurado no .env");
 }
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.GMAIL_ACCOUNT,
+    pass: process.env.GMAIL_PASSWORD
+  }
+});
 
 const MainRouter = express.Router();
 
@@ -23,5 +34,48 @@ MainRouter.use("/feedbacks", feedbackRouter);
 MainRouter.get("/", (_req, res) => {
     res.json({ msg: "Ybitu API online" });
 });
+
+MainRouter.get("/", (req, res) => {
+  res.json({ msg: "Oiii" });
+});
+
+MainRouter.get("/auth", Auth.private, (req, res) => {
+  res.status(201).json({
+    logged: true,
+  })
+})
+
+MainRouter.get("/logout", Auth.private, (req, res) => {
+  res.clearCookie("token")
+  res.status(201).json({
+    logged: false
+  })
+})
+
+MainRouter.post("/email", (req, res) => { 
+  console.log(req.body )
+  transporter.sendMail({
+    from: process.env.GMAIL_ACCOUNT,
+    to: process.env.GMAIL_ACCOUNT,
+    replyTo: req.body.email,
+    subject: `Registro de Contato - Dúvidas de ${req.body.nome}`,
+    text: `
+    Pessoa que entrou em contato: ${req.body.nome}
+    Data de contato: ${new Date().toLocaleDateString("pt-BR")}
+    Formas de Contato: 
+      email: ${req.body.email},
+      telefone: ${req.body.telefone}
+    
+    Dúvida de ${req.body.nome}:
+      ${req.body.comentario}
+    `
+  }).then(() => {
+    res.json({ msg: "Deu Certooo" })
+  }).catch(err => {
+    console.log(err)
+    res.json({ msg: "Deu merda parceiro" })
+  })
+});
+
 
 export default MainRouter;
