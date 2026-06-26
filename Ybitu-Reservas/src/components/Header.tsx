2ae8ace72a1792c83f4603ec7856d470c1b1b1b4
MATHEUS <matheus@debian-mvzza.jidmt;eyh>
2026-06-26 10:27:19 -0300

    Remove button

--- Ybitu-Reservas/src/components/Header.tsx
+++ Ybitu-Reservas/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Menu, X } from "lucide-react";
 import { motion, AnimatePresence } from 'framer-motion';
 import { useAuth } from '../context/AuthContext'
 import logo from '../assets/logo.png'
-import seta from '../assets/seta_carrinho.svg'
+
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -21,7 +21,6 @@ export default function Header() {
 
     const publicLinks = [
         { to: "/", label: "Home" },
-        { to: "/Pousada", label: "Pousada Ybitu" },
         { to: "/Reserva/Data", label: "Reserva" },
         { to: "/Contato", label: "Contato" },
     ];
@@ -54,13 +53,7 @@ export default function Header() {
             </nav>
 
             {/* Botão Carrinho Desktop */}
-            <button className="hidden lg:flex shrink-0 flex-row items-center gap-3 hover:cursor-pointer bg-green-800 text-white font-bold py-3 px-5 xl:px-7 rounded-lg relative overflow-hidden group transition-colors duration-300 hover:bg-green-700">
-                <div className="absolute inset-0 w-1/2 h-full bg-linear-to-r from-transparent via-white/30 to-transparent -skew-x-12 transform -translate-x-full group-hover:translate-x-[200%] transition-transform duration-500 ease-out z-0" />
-                <span className="bg-amber-300 w-8 h-8 rounded-sm z-10 shrink-0 flex items-center justify-center">
-                    <img src={seta} alt="Ícone do carrinho" className="w-5 h-5" />
-                </span>
-                <span className="relative z-10 text-sm xl:text-base whitespace-nowrap">VER CARRINHO</span>
-            </button>
+           
 
             {/* Botão Hamburguer */}
             <button
@@ -80,12 +73,7 @@ export default function Header() {
                         transition={{ duration: 0.3, ease: "easeInOut" }}
                         className="absolute top-full left-0 w-full bg-black/90 flex flex-col items-start gap-5 px-6 py-6 lg:hidden z-50"
                     >
-                        <button className="flex items-center gap-4 w-full bg-green-800 text-white font-bold py-3 px-5 rounded-lg mb-2">
-                            <span className="bg-amber-300 w-8 h-8 rounded-sm shrink-0 flex items-center justify-center">
-                                <img src={seta} alt="Ícone do carrinho" className="w-5 h-5" />
-                            </span>
-                            <span>VER CARRINHO</span>
-                        </button>
+                        
 
                         {allLinks.map(({ to, label }) => (
                             <Link
